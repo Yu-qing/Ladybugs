08717955dab2f72b77bd9fb8a6dbb2e50b0d5aa5
xeriop128852 <xeriop128852@gmail.com>
2021-12-01 00:02:22 +0800

    update dataset

---
+++ dataset/test_wild.py
@@ -0,0 +1,15 @@
+import csv
+import os
+import time
+
+start = time.time()
+
+path = 'smartbugs_wild.csv'
+with open(path, newline='') as csvfile:
+    rows = csv.reader(csvfile, delimiter=',')
+    
+    for row in rows:
+        os.system("slither {0}".format(row[0]))
+
+end = time.time()
+print("spend time : ", end-start, "(s)")
